0f765fc14fd9f70f729ada1336f08f3b03bd0242
Laurenz Rausche <laurenz@laurenz-rausche.de>
2024-04-28 10:14:59 +0000

    fix http code of successfull refresh

--- express/src/routes/refresh.ts
+++ express/src/routes/refresh.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import { PassedProps } from "../authInstance";
-import { sendAuthError, sendAuthServerError } from "../senders";
+import { sendAuthError, sendAuthResponse, sendAuthServerError } from "../senders";
+import { decodeToken, generateToken } from "../tokenUtils";
 
 export default function ({
   config,
@@ -16,7 +17,40 @@ export default function ({
     }
 
     try {
-      // CODE HERE
+      const { refreshToken }: { refreshToken: string } = req.body;
+
+      if (!refreshToken) {
+        log("debug", "REFRESHTOKEN MISSING");
+        return sendAuthError(res, 400, 42);
+      }
+
+      const { valid, payload } = decodeToken(
+        refreshToken,
+        config.refreshTokenSecret
+      );
+
+      if (!valid || !payload) {
+        log("debug", "THE REFRESHTOKEN IS INVALID");
+        return sendAuthError(res, 403, 43);
+      }
+
+      const { serverError: checkTokenServerError, exists } =
+        await useEventCallbacks.checkToken({ token: refreshToken });
+
+      if (checkTokenServerError) return sendAuthServerError(res);
+
+      if (!exists) {
+        log("debug", "THE REFRESHTOKEN DOES NOT EXIST");
+        return sendAuthError(res, 404, 44);
+      }
+
+      const accessToken = generateToken(
+        { id: payload.id },
+        config.accessTokenSecret,
+        config.expiresIn
+      );
+
+      return sendAuthResponse(res, 201, )
     } catch (error) {
       log("warn", String(error));
       return sendAuthServerError(res);
